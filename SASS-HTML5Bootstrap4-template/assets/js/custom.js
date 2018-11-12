e101b55812d1a3c676e96eff43ba0c5ae580314c
LAPTOP-LC18HGB0\HP PC <kamalrock23@gmail.com>
2018-11-12 22:34:17 +0530

    Dashboard pages added

--- SASS-HTML5Bootstrap4-template/assets/js/custom.js
+++ SASS-HTML5Bootstrap4-template/assets/js/custom.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
+	$('.sidebar-menu-btn').click(function(){
+		$('.slidebar').toggleClass('open');
+		$('.has-sidebar').toggleClass('open');
+		$(this).toggleClass('menu-open');
+	});
+
+	$('.menu-links li a').click(function(){
+		if($(this).parent().hasClass('active')){
+			$(this).next('ul').slideUp();
+			$(this).parent().removeClass('active');
+		}
+		else{
+			$(this).next('ul').slideDown();
+			$(this).parent().addClass('active');
+		}
+	});
+
 });
 
 
